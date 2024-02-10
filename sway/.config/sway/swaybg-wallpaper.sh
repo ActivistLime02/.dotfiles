7ab9e2da3a5eab9f3daa7ed3968224c1fba4eb55
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2024-02-10 14:02:47 +0100

    Fixed monitor settings and fixed flameshot

--- sway/.config/sway/swaybg-wallpaper.sh
+++ sway/.config/sway/swaybg-wallpaper.sh
@@ -2,11 +2,15 @@
 
 FOLDERS="/home/nick/Pictures/achtergronden-4K-jxl/ /home/nick/Pictures/Azur_Lane /home/nick/Pictures/Safe?"
 
-swaybg -o "DP-2" -i $(find $FOLDERS -type f | shuf -n1) -m fill -o "HDMI-A-2" -i $(find $FOLDERS -type f | shuf -n1) -m fill &
+swaybg -o "LG Electronics LG TV SSCR2 0x00000101" -i $(find $FOLDERS -type f | shuf -n1) -m fill \
+    -o "Acer Technologies R271 T60EE0032410" -i $(find $FOLDERS -type f | shuf -n1) -m fill \
+    &
 OLD_PID=$!
 while true; do
     sleep 30
-    swaybg -o "DP-2" -i $(find $FOLDERS -type f | shuf -n1) -m fill -o "HDMI-A-2" -i $(find $FOLDERS -type f | shuf -n1) -m fill &
+    swaybg -o "LG Electronics LG TV SSCR2 0x00000101" -i $(find $FOLDERS -type f | shuf -n1) -m fill \
+        -o "Acer Technologies R271 T60EE0032410" -i $(find $FOLDERS -type f | shuf -n1) -m fill \
+        &
     NEXT_PID=$!
     sleep 5
     kill $OLD_PID
