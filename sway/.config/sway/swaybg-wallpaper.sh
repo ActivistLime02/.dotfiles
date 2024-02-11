0c1a1b628194de8e8805010f126bda4e5a08cdc6
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2024-02-11 09:40:41 +0100

    laptop screen added in backgrounds

--- sway/.config/sway/swaybg-wallpaper.sh
+++ sway/.config/sway/swaybg-wallpaper.sh
@@ -4,12 +4,14 @@ FOLDERS="/home/nick/Pictures/achtergronden-4K-jxl/ /home/nick/Pictures/Azur_Lane
 
 swaybg -o "LG Electronics LG TV SSCR2 0x00000101" -i $(find $FOLDERS -type f | shuf -n1) -m fill \
     -o "Acer Technologies R271 T60EE0032410" -i $(find $FOLDERS -type f | shuf -n1) -m fill \
+    -o "Najing CEC Panda FPD Technology CO. ltd 0x0036 Unknown" -i $(find $FOLDERS -type f | shuf -n1) -m fill \
     &
 OLD_PID=$!
 while true; do
     sleep 30
     swaybg -o "LG Electronics LG TV SSCR2 0x00000101" -i $(find $FOLDERS -type f | shuf -n1) -m fill \
         -o "Acer Technologies R271 T60EE0032410" -i $(find $FOLDERS -type f | shuf -n1) -m fill \
+        -o "Najing CEC Panda FPD Technology CO. ltd 0x0036 Unknown" -i $(find $FOLDERS -type f | shuf -n1) -m fill \
         &
     NEXT_PID=$!
     sleep 5
