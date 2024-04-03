f9f55844bbd76021abe8c427248ef7907120a429
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2024-04-03 16:18:15 +0200

    Add unlock for wallet and ssh keys

--- sway.sh
+++ sway.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+eval $(keychain --eval --quiet id_ed25519 ~/.ssh/lvl27-nick)
 val=$(udevadm info -a -n /dev/dri/card1 | grep boot_vga | rev | cut -c 2)
 WRL_DRM_DEVICES="/dev/dri/card$val" \
     XDG_CURRENT_DESKTOP=sway \
