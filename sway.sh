2cadd419f589ae1d2b87008ed6fcdd75ff839f47
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2024-06-22 11:44:58 +0200

    Commented out, it did weird things

--- sway.sh
+++ sway.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#GTK_THEME="Catppuccin-Frappe-Standard-Rosewater-dark:dark" \
+#QT_WAYLAND_SHELL_INTEGRATION=xdg-shell \
 #eval $(keychain --eval --quiet id_ed25519 ~/.ssh/lvl27-nick)
 val=$(udevadm info -a -n /dev/dri/card1 | grep boot_vga | rev | cut -c 2)
 WRL_DRM_DEVICES="/dev/dri/card$val" \
