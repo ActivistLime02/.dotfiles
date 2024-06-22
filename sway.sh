0a8a042f841632dbbadaed45f2bf16ec8fcd65e0
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2024-06-22 11:44:24 +0200

    Changed SSH key password prompt

--- sway.sh
+++ sway.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-eval $(keychain --eval --quiet id_ed25519 ~/.ssh/lvl27-nick)
+#eval $(keychain --eval --quiet id_ed25519 ~/.ssh/lvl27-nick)
 val=$(udevadm info -a -n /dev/dri/card1 | grep boot_vga | rev | cut -c 2)
 WRL_DRM_DEVICES="/dev/dri/card$val" \
     XDG_CURRENT_DESKTOP=sway \
@@ -8,4 +8,6 @@ WRL_DRM_DEVICES="/dev/dri/card$val" \
     _JAVA_AWT_WM_NONREPARENTING=1 \
     QT_QPA_PLATFORM=wayland \
     XDG_SESSION_DESKTOP=sway \
+    SSH_ASKPASS=/usr/bin/ksshaskpass \
+    SSH_ASKPASS_REQUIRE=prefer \
     dbus-run-session sway --unsupported-gpu
