d57585fddb5204383344eb59a4655bd6d68009fe
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2024-08-27 08:59:18 +0200

    Random changes

--- sway.sh
+++ sway.sh
@@ -5,7 +5,7 @@
 val=$(udevadm info -a -n /dev/dri/card1 | grep boot_vga | rev | cut -c 2)
 WRL_DRM_DEVICES="/dev/dri/card$val" \
     XDG_CURRENT_DESKTOP=sway \
-    QT_QPA_PLATFORMTHEME=qt6ct \
+    QT_QPA_PLATFORMTHEME=qt5ct:qt6ct \
     SDL_VIDEODRIVER=wayland \
     _JAVA_AWT_WM_NONREPARENTING=1 \
     QT_QPA_PLATFORM=wayland \
