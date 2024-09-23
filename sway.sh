696fe8acf1f0a9dca78eba70a876790d64cb69d1
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2024-09-23 10:38:36 +0200

    Fixed some things with fcitx

--- sway.sh
+++ sway.sh
@@ -3,7 +3,9 @@
 #QT_WAYLAND_SHELL_INTEGRATION=xdg-shell \
 #eval $(keychain --eval --quiet id_ed25519 ~/.ssh/lvl27-nick)
 val=$(udevadm info -a -n /dev/dri/card1 | grep boot_vga | rev | cut -c 2)
-WRL_DRM_DEVICES="/dev/dri/card$val" \
+    WRL_DRM_DEVICES="/dev/dri/card$val" \
+    QT_IM_MODULE=fcitx \
+    XMODIFIERS=@im=fcitx \
     XDG_CURRENT_DESKTOP=sway \
     QT_QPA_PLATFORMTHEME=qt5ct:qt6ct \
     SDL_VIDEODRIVER=wayland \
