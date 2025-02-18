1920fce5352b9fd2d1fc72403352ee819ef57a00
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2025-02-18 10:25:47 +0100

    Reasonably good SQL query system, dadbod

---
+++ nvim/.config/nvim/lua/nick/plugins/mason.lua
@@ -0,0 +1,7 @@
+return {
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+    --- Hieronder is voor debuggers, ik zoek dat een ander keer wel uit.
+    --- { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
+    {'mfussenegger/nvim-lint'}
+}
