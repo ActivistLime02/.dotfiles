8088b681ac5d997e093497897ea3898101ffe647
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2025-03-26 10:26:24 +0100

    dadbod fixed

---
+++ nvim/.config/nvim/lua/nick/plugins/dadbod.lua
@@ -0,0 +1,27 @@
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    -- Your DBUI configuration
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.dbs = {
+        -- { name = 'lokale mysql', url = 'mysql://username:password@localhost' },
+    }
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "dbout",
+        callback = function()
+            vim.opt_local.foldenable = false
+        end,
+    })
+  end,
+}
+
